9778cbbeaabd333f0d088a37b6ca3569a573e321
Halton Huo <halton.huo@gmail.com>
2018-03-09 17:36:26 +0800

    Use 2-space as indent.

--- bin/build_chromium.js
+++ bin/build_chromium.js
@@ -4,30 +4,30 @@ const program = require('commander');
 const { ChromeBuilder } = require('../src/chrome_builder');
 
 program
-	.version(require('../package.json').version)
-	.usage('sub-command [options] <dir>')
-	.option('-t, --build-type <buildType>', 'Build Type', /^(debug|release)$/i, 'release')
-	// Get list via "gn help target_os <out_dir>"
-	.option('--target-os <targetOs>', 'Target OS', /^(android|chromeos|linux|nacl|mac|win)$/i, null)
-	// Get list via "gn help target_cpu <out_dir>"
-	.option('--target-cpu <targetCpu>', 'Target CPU', /^(x86|x64|arm|arm64|mipsel)$/i, null)
-	.option('--root-dir <rootDir>', 'Chromium source code directory', process.cwd())
-	.parse(process.argv);
+  .version(require('../package.json').version)
+  .usage('sub-command [options] <dir>')
+  .option('-t, --build-type <buildType>', 'Build Type', /^(debug|release)$/i, 'release')
+  // Get list via "gn help target_os <out_dir>"
+  .option('--target-os <targetOs>', 'Target OS', /^(android|chromeos|linux|nacl|mac|win)$/i, null)
+  // Get list via "gn help target_cpu <out_dir>"
+  .option('--target-cpu <targetCpu>', 'Target CPU', /^(x86|x64|arm|arm64|mipsel)$/i, null)
+  .option('--root-dir <rootDir>', 'Chromium source code directory', process.cwd())
+  .parse(process.argv);
 
 let builder = new ChromeBuilder(program.args[0],
-																program.args[1],
-																program.buildType,
-																program.targetOs,
-																program.targetCpu);
+                                program.args[1],
+                                program.buildType,
+                                program.targetOs,
+                                program.targetCpu);
 
 if (!builder.isSupportedSubCommand()) {
-	console.log(`Unsupported sub-command ${builder.subCommand}`);
-	process.exit(1);
+  console.log(`Unsupported sub-command ${builder.subCommand}`);
+  process.exit(1);
 }
 
 if (!builder.validateRootDir()) {
-	console.log(`Invalid chrome source dir: ${builder.rootDir}`);
-	process.exit(1);
+  console.log(`Invalid chrome source dir: ${builder.rootDir}`);
+  process.exit(1);
 }
 
 builder.run();
--- src/chrome_builder.js
+++ src/chrome_builder.js
@@ -7,148 +7,148 @@ const spawn = require('child_process').spawn;
 const path = require('path');
 
 class ChromeBuilder {
-	constructor(subCommand, rootDir, targetOs, targetCpu, buildType) {
-		this.supportedSubCommands = ['sync', 'config', 'build', 'package', 'upload'];
+  constructor(subCommand, rootDir, targetOs, targetCpu, buildType) {
+    this.supportedSubCommands = ['sync', 'config', 'build', 'package', 'upload'];
 
-		this.subCommand = subCommand;
-		this.rootDir = path.resolve(rootDir);
-		this.buildType = buildType;
+    this.subCommand = subCommand;
+    this.rootDir = path.resolve(rootDir);
+    this.buildType = buildType;
 
-		this.hostOs = getHostOs();
-		this.hostCpu = getHostCpu();
-		this.targetOs = targetOs ? targetOs : this.hostOs;
-		this.targetCpu = targetCpu ? targetCpu : this.hostCpu;
+    this.hostOs = getHostOs();
+    this.hostCpu = getHostCpu();
+    this.targetOs = targetOs ? targetOs : this.hostOs;
+    this.targetCpu = targetCpu ? targetCpu : this.hostCpu;
 
-		this.outDir = path.join(this.rootDir, 'out',
-														this.targetOs + '_' + this.targetCpu + '_' + this.buildType);
+    this.outDir = path.join(this.rootDir, 'out',
+                            this.targetOs + '_' + this.targetCpu + '_' + this.buildType);
 
 
-		this.gnArgs = 'is_debug=' + (this.buildType == 'debug').toString();
-	}
+    this.gnArgs = 'is_debug=' + (this.buildType == 'debug').toString();
+  }
 
-	isSupportedSubCommand() {
-		return this.supportedSubCommands.includes(this.subCommand);
-	}
+  isSupportedSubCommand() {
+    return this.supportedSubCommands.includes(this.subCommand);
+  }
 
-	validateRootDir() {
-		try {
-			fs.accessSync(this.rootDir);
-			fs.accessSync(path.resolve(this.rootDir, 'chrome', 'VERSION'));
-		} catch (e) {
-			return false;
-		}
+  validateRootDir() {
+    try {
+      fs.accessSync(this.rootDir);
+      fs.accessSync(path.resolve(this.rootDir, 'chrome', 'VERSION'));
+    } catch (e) {
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	run() {
-		switch (this.subCommand) {
-			case 'sync':
-				this.gclientSync();
-				break;
-			case 'config':
-				this.runGN();
-				break;
-			case 'build':
-				this.ninjaBuild();
-				break;
-			case 'package':
-				this.runPackage();
-				break;
-			case 'upload':
-				this.runUpload();
-				break;
-		}
-	}
-	gclientSync() {
-		execCommand('gclient', ['sync'], this.rootDir);
-	}
+  run() {
+    switch (this.subCommand) {
+      case 'sync':
+        this.gclientSync();
+        break;
+      case 'config':
+        this.runGN();
+        break;
+      case 'build':
+        this.ninjaBuild();
+        break;
+      case 'package':
+        this.runPackage();
+        break;
+      case 'upload':
+        this.runUpload();
+        break;
+    }
+  }
+  gclientSync() {
+    execCommand('gclient', ['sync'], this.rootDir);
+  }
 
-	runGN() {
-		execCommand('gn', ['gen', `--args=${this.gnArgs}`, this.outDir], this.rootDir);
-	}
+  runGN() {
+    execCommand('gn', ['gen', `--args=${this.gnArgs}`, this.outDir], this.rootDir);
+  }
 
-	ninjaBuild() {
+  ninjaBuild() {
 
-	}
+  }
 
-	runPackage() {
+  runPackage() {
 
-	}
+  }
 
-	runUpload() {
+  runUpload() {
 
-	}
+  }
 }
 
 // Get host OS
 function getHostOs() {
-	let hostOs = os.platform();
-	switch(hostOs) {
-		case 'linux':
-			break;
-		case 'win32':
-			program.targetOs = 'win';
-			break;
-		case 'darwin':
-			program.targetOs = 'mac';
-			break;
-		case 'aix':
-		case 'freebsd':
-		case 'openbsd':
-		case 'sunos':
-			program.targetOs = 'linux';
-			break;
+  let hostOs = os.platform();
+  switch(hostOs) {
+    case 'linux':
+      break;
+    case 'win32':
+      program.targetOs = 'win';
+      break;
+    case 'darwin':
+      program.targetOs = 'mac';
+      break;
+    case 'aix':
+    case 'freebsd':
+    case 'openbsd':
+    case 'sunos':
+      program.targetOs = 'linux';
+      break;
 
-	}
-	return hostOs;
+  }
+  return hostOs;
 }
 
 // Get Default ARCH
 function getHostCpu() {
-	let hostCpu = os.arch();
-	switch(hostCpu) {
-		case 'arm':
-		case 'arm64':
-		case 'mipsel':
-		case 'x64':
-			break;
-		case 'ia32':
-			hostCpu = 'x86';
-		case 'mips':
-		case 'ppc':
-		case 'ppc64':
-		case 's390':
-		case 's390x':
-		case 'x32':
-			console.error(`Unsuppurted arch: ${program.targetCpu}`);
-	}
+  let hostCpu = os.arch();
+  switch(hostCpu) {
+    case 'arm':
+    case 'arm64':
+    case 'mipsel':
+    case 'x64':
+      break;
+    case 'ia32':
+      hostCpu = 'x86';
+    case 'mips':
+    case 'ppc':
+    case 'ppc64':
+    case 's390':
+    case 's390x':
+    case 'x32':
+      console.error(`Unsuppurted arch: ${program.targetCpu}`);
+  }
 
-	return hostCpu;
+  return hostCpu;
 }
 
 function execCommand(cmd, args, workingDir) {
-	chdir(workingDir, () => {
+  chdir(workingDir, () => {
 
-		const cmdFullStr = cmd + ' ' + args.join(' ');
-		const exec = spawn(cmd, [...args]);
+    const cmdFullStr = cmd + ' ' + args.join(' ');
+    const exec = spawn(cmd, [...args]);
 
-		exec.stdout.on('data', (data) => {
-		  console.log(`${data}`);
-		});
+    exec.stdout.on('data', (data) => {
+      console.log(`${data}`);
+    });
 
-		exec.stderr.on('data', (data) => {
-		  console.log(`${data}`);
-		});
+    exec.stderr.on('data', (data) => {
+      console.log(`${data}`);
+    });
 
-		exec.on('close', (code) => {
-			if (code !== 0)
+    exec.on('close', (code) => {
+      if (code !== 0)
         console.log(`\"${cmdFullStr}\" exited with code ${code}`);
-		});
+    });
 
-	});
+  });
 }
 
 module.exports = {
-	ChromeBuilder
+  ChromeBuilder
 }
